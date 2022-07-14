ca5789f1792124425be6e68f53e9d0682f1eeb94
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-13 20:13:26 -0400

    study

--- server.js
+++ server.js
@@ -22,4 +22,18 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
